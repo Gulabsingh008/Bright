255ef90343b1308e2e95ea83cb9e992ad0624c07
AF <gulabsinghrajput008@gmail.com>
2025-01-21 11:19:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH")
     AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-    HOST = "https://drm-api-six.vercel.app"
+    HOST = "https://drm-new-5a2b59e50b26.herokuapp.com"
     CREDIT = "𝗥𝗼𝘆 𝗝𝗮𝗮𝘁™"#Here You Can Change with Your Name  or any custom name or title you prefer
